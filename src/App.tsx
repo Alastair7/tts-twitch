4bc561d7ad042d7a4badeae20674db1d8983d5f2
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-01-24 20:09:58 +0100

    add init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Twitch TTS</h1>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
